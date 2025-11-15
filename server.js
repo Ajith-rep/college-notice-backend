dc1e24c25929d08295ca0e8606cf6d474563ef84
Ajith-rep <khambhamajith@gmail.com>
2025-11-15 23:00:41 +0530

    Added PORT for Render

--- server.js
+++ server.js
@@ -46,4 +46,7 @@ app.delete("/notices/:id", (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server running on port")
+})
